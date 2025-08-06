3fb755a2ae0bc223089952d522f1990d63b9f6aa
Vedant Raut <vedantraut49@gmail.com>
2025-08-06 22:48:18 +0530

    adding books component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { BooksComponent } from './components/books/books.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: BooksComponent }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
