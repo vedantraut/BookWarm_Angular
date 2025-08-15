0c718effcd20b9a461eaf9c95eb01490142d7648
Vedant Raut <vedantraut49@gmail.com>
2025-08-15 16:41:30 +0530

    Search books functionality added

--- src/app/components/books/books.component.ts
+++ src/app/components/books/books.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { BookService } from '../../services/book.service';
 import { CommonModule } from '@angular/common';
 import { BookDTO } from '../../models/bookDTO';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-books',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css'],
 })
@@ -14,11 +15,26 @@ export class BooksComponent {
   constructor(private bookservice: BookService) {}
 
   booksData: BookDTO[] = [];
+  filteredBooksData: BookDTO[] = [];
+  searchText: string = '';
 
   ngOnInit() {
     this.bookservice.getAllBooks().subscribe((data) => {
       console.log('all the books -- ', data);
       this.booksData = data as any[];
+      this.filteredBooksData = [...this.booksData];
     });
+  }
+
+  get filteredBooks(): BookDTO[] {
+    if (!this.searchText) {
+      return this.booksData;
+    }
+
+    this.filteredBooksData = this.booksData.filter((book) =>
+      book.title.toLowerCase().startsWith(this.searchText.toLowerCase())
+    );
+
+    return this.filteredBooksData;
   }
 }
