e21f3ebb87eccb93e22d95ceb433005423792ce9
Vedant Raut <vedantraut49@gmail.com>
2025-08-16 17:19:53 +0530

    Added search functionality for coffee

--- src/app/components/coffee/coffee.component.ts
+++ src/app/components/coffee/coffee.component.ts
@@ -3,10 +3,11 @@ import { CoffeeService } from '../../services/coffee.service';
 import { CoffeeDTO } from '../../models/coffeeDTO';
 import { RouterModule } from '@angular/router';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-coffee',
-  imports: [RouterModule, NavBarComponent],
+  imports: [FormsModule, RouterModule, NavBarComponent],
   templateUrl: './coffee.component.html',
   styleUrl: './coffee.component.css',
 })
@@ -14,13 +15,33 @@ export class CoffeeComponent {
   constructor(private coffeeService: CoffeeService) {}
 
   coffeeData: CoffeeDTO[] = [];
+  filteredCoffeesData: CoffeeDTO[] = [];
+  searchText: string = '';
 
   ngOnInit() {
     this.coffeeService.getCoffees().subscribe((data) => {
-      console.log('All the coffees in the db are -- ' + data);
+      console.log('All the coffees in the db are -- ', data);
 
-      this.coffeeData = data as any[];
-      console.log('All the CoffeeDTO -- ' + this.coffeeData);
+      this.coffeeData = data as CoffeeDTO[];
+      this.filteredCoffeesData = [...this.coffeeData];
+
+      // For debugging purposes
+      console.log('All the CoffeeDTO -- ', this.coffeeData);
     });
+  }
+
+  get filteredCoffees(): CoffeeDTO[] {
+    if (!this.searchText) {
+      return this.coffeeData;
+    }
+    console.log('Search Text -- ' + this.searchText);
+
+    return this.coffeeData.filter((coffee) =>
+      coffee.name.toLowerCase().startsWith(this.searchText.toLowerCase())
+    );
+
+    console.log('Filtered Coffees -- ' + this.filteredCoffeesData);
+
+    // return this.filteredCoffeesData;
   }
 }
